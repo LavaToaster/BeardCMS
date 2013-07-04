76db6dfc4b64c63dde3c1995f20ea7f95f53dbb2
Adam Lavin <adam@lavoaster.co.uk>
2013-07-04 03:09:59 +0100

    Users Model now extends Sentry's model

--- beardcms/models/Users.php
+++ beardcms/models/Users.php
@@ -1,56 +1,7 @@
 <?php
 
-use Illuminate\Auth\UserInterface;
-use Illuminate\Auth\Reminders\RemindableInterface;
+class Users extends Cartalyst\Sentry\Users\Eloquent\User {
 
-class Users extends Eloquent implements UserInterface, RemindableInterface {
-
-    public $email;
-
-    protected $password;
-
-    /**
-	 * The database table used by the model.
-	 *
-	 * @var string
-	 */
-	protected $table = 'users';
-
-	/**
-	 * The attributes excluded from the model's JSON form.
-	 *
-	 * @var array
-	 */
-	protected $hidden = ['password'];
-
-	/**
-	 * Get the unique identifier for the user.
-	 *
-	 * @return mixed
-	 */
-	public function getAuthIdentifier()
-	{
-		return $this->getKey();
-	}
-
-	/**
-	 * Get the password for the user.
-	 *
-	 * @return string
-	 */
-	public function getAuthPassword()
-	{
-		return $this->password;
-	}
-
-	/**
-	 * Get the e-mail address where password reminders are sent.
-	 *
-	 * @return string
-	 */
-	public function getReminderEmail()
-	{
-		return $this->email;
-	}
+    //TODO Custom User functions
 
 }
