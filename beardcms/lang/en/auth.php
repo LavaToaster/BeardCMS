b003873e6ce7bb00c457e76c92529dd46e3572f5
Adam Lavin <adam@lavoaster.co.uk>
2013-07-04 17:46:11 +0100

    Added auth language file

---
+++ beardcms/lang/en/auth.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'login_required'        => 'Login field is required.',
+    'missing_password'      => 'You forgot to enter a password.',
+    'wrong_password'        => 'Sorry but the password entered is incorrect.',
+    'account_not_found'     => 'Sorry but this account was not found.',
+    'account_not_activated' => 'Sorry but this account has not been activated.',
+    'account_suspended'     => 'Sorry but this account has been suspended.',
+    'account_banned'        => 'Sorry but this account has been banned.'
+];
