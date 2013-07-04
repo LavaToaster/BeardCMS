62b349672445fd8052b3b416113563f6497f2cbe
Adam Lavin <adam@lavoaster.co.uk>
2013-07-04 03:16:00 +0100

    Temporarily using the public master layout

--- beardcms/controllers/AdminController.php
+++ beardcms/controllers/AdminController.php
@@ -3,9 +3,10 @@
 class AdminController extends BaseController {
 
     /**
+     * @note Temp until the admin template is ready
      * @var Illuminate\View\View
      */
-    protected $layout = "layouts.admin.master";
+    protected $layout = "layouts.master";
 
     public function __construct() {
         //TODO Validate user's auth
