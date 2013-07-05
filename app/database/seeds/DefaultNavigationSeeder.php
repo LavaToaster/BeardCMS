cb819404f1a30f272b4b595fc8e01ef0a3ed70c7
Adam Lavin <adam@lavoaster.co.uk>
2013-07-05 11:23:23 +0100

    Added some admin navigation items to the seeder

--- app/database/seeds/DefaultNavigationSeeder.php
+++ app/database/seeds/DefaultNavigationSeeder.php
@@ -4,10 +4,33 @@ class DefaultNavigationSeeder extends Seeder
 {
     public function run()
     {
-        $nav = new Navigation();
-        $nav->name  = "Home";
-        $nav->title = "Go to the homepage";
-        $nav->url   = Config::get('app.url');
-        $nav->save();
+        /* Default Admin */
+
+        //Dashboard
+        Navigation::insert([
+            'name'  => 'Dashboard',
+            'title' => 'Go to the dashboard',
+            'url'   => 'admin/dashboard',
+            'type'  => 2
+        ]);
+
+        //Pages
+        Navigation::insert([
+            'name'  => 'Pages',
+            'title' => 'Go to the page lsit',
+            'url'   => 'admin/pages',
+            'type'  => 2
+        ]);
+
+
+        /* Default Public */
+
+        //Home
+        Navigation::insert([
+            'name'  => 'Home',
+            'title' => 'Go to the homepage',
+            'url'   => Config::get('app.url'),
+            'type'  => 1
+        ]);
     }
 }
