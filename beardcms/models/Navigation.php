dd18f3db5460e6a435c344aa30dd2db975db1c69
Adam Lavin <adam@lavoaster.co.uk>
2013-07-05 08:54:08 +0100

    Added phpDoc to isActive()

--- beardcms/models/Navigation.php
+++ beardcms/models/Navigation.php
@@ -4,6 +4,11 @@ class Navigation extends Eloquent {
 
     protected $table = 'navigation';
 
+    /**
+     * Checks if the selected navigation item is currently active
+     *
+     * @return bool
+     */
     public function isActive()
     {
         return $this->url == Url::current() ? true : false;
