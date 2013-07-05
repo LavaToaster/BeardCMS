c9209ec8edb5c82b0f335b3bae50abbb7fe0d222
Adam Lavin <adam@lavoaster.co.uk>
2013-07-05 09:10:26 +0100

    Modified DefaultPageSeeder to fill using model attributes now

--- beardcms/database/seeds/DefaultPageSeeder.php
+++ beardcms/database/seeds/DefaultPageSeeder.php
@@ -2,19 +2,17 @@
 
 class DefaultPageSeeder extends Seeder {
     public function run() {
-        $data = [
-            'title' => 'Home',
-            'slug'   => 'index.html',
-            'content' => '<div id="full-page">
+        $page = new Page;
+
+        $page->title   = 'Home';
+        $page->slug    = 'index.html';
+        $page->content = '<div id="full-page">
     <div class="hero-unit">
         <h1>Hello World!</h1>
         <p>Welcome to the default BeardCMS page.</p>
     </div>
-</div>'
-        ];
+</div>';
 
-        $page = new Page;
-        $page->fill($data);
         $page->save();
     }
 }
