c5ad14721a3555aaa1b9ce6d2134e33f047acbd3
Adam Lavin <adam@lavoaster.co.uk>
2013-07-05 07:02:25 +0100

    Added very basic AdminDashboardController

---
+++ beardcms/controllers/admin/dashboard/AdminDashboard.php
@@ -0,0 +1,9 @@
+<?php
+
+class AdminDashboardController extends AdminController
+{
+    public function getIndex()
+    {
+        $this->layout->content = View::make('admin.dashboard.dashboard');
+    }
+}
---
+++ beardcms/views/admin/dashboard/dashboard.blade.php
@@ -0,0 +1,9 @@
+@section("title")
+Dashboard | BeardCMS ACP
+@endsection
+
+@section("content")
+<div class="container">
+    Dashboard Placeholder
+</div>
+@endsection
