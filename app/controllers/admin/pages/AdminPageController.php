8d6ffa41e0cee88cf3e21c24e3a7492e620ba426
Adam Lavin <adam@lavoaster.co.uk>
2013-08-07 18:43:43 +0100

    Added the ability to edit pages

--- app/controllers/admin/pages/AdminPageController.php
+++ app/controllers/admin/pages/AdminPageController.php
@@ -8,4 +8,24 @@ class AdminPageController extends AdminController
 
         $this->layout->content = View::make('admin.page.list')->with('pages', $pages);
     }
+
+    public function edit($id)
+    {
+        $page = Page::findOrFail($id);
+        $this->layout->content = View::make('admin.page.edit')->with('page', $page);
+    }
+
+    public function update($id)
+    {
+        $page = Page::findOrFail($id);
+
+        $page->title   = Input::input('title');
+        $page->slug    = Input::input('slug');
+        $page->content = Input::input('content');
+
+        $page->save();
+
+        return Redirect::to("/admin/page")->with('message', 'The page was edited successfully.')->with('message-type', 'alert-success');
+    }
+
 }
---
+++ app/views/admin/page/edit.blade.php
@@ -0,0 +1,36 @@
+@section("title")
+Edit Page | BeardCMS ACP
+@endsection
+
+@section("content")
+<div class="container">
+    <div class="row">
+        <div class="col-12 well">
+            <fieldset>
+                <legend>Editing {{ $page->title }}</legend>
+                {{ Form::model($page, array('method' => 'put', 'route' => ['admin.page.update', $page->id], 'class' => 'form-horizontal')) }}
+                <div class="form-group">
+                    <label class="col-lg-2 control-label" for="title">Title</label>
+                    <div class="col-lg-10">
+                        {{ Form::text('title', $page->title, ['class' => 'form-control', 'placeholder' => 'E.G: Home...']) }}
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="col-lg-2 control-label" for="slug">Slug</label>
+                    <div class="col-lg-10">
+                        {{ Form::text('slug', $page->slug, ['class' => 'form-control', 'placeholder' => 'home.html']) }}
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="col-lg-2 control-label" for="content">Content</label>
+                    <div class="col-lg-10">
+                        {{ Form::textarea('content', $page->content, ['class' => 'form-control']) }}
+                    </div>
+                </div>
+                <button type="submit" name="submit" class="btn btn-info btn-block btn-large">Edit Page</button>
+                {{ Form::close(); }}
+            </fieldset>
+        </div>
+    </div>
+</div>
+@endsection
