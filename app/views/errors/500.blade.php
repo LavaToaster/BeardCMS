9195c1dc6ec7c943e9dbb6b86f69d997494bbe39
Adam Lavin <adam@lavoaster.co.uk>
2013-08-07 17:58:47 +0100

    Updated error pages to use the jumbotron class, they also no longer extend layouts.master

--- app/views/errors/500.blade.php
+++ app/views/errors/500.blade.php
@@ -1,12 +1,10 @@
-@extends("layouts.master")
-
 @section("title")
 Error | BeardCMS
 @endsection
 
 @section("content")
 <div id="full-page">
-    <div class="hero-unit">
+    <div class="jumbotron">
         <h1>Error: 500 Internal Server Error.</h1>
         <p>There seems to be an error processing this request.</p>
     </div>
