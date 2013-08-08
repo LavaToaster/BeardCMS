6d9900059133a42495f2654c024378e9b83a4017
Adam Lavin <adam@lavoaster.co.uk>
2013-08-08 01:08:50 +0100

    Added the ability to create pages

--- app/controllers/admin/pages/AdminPageController.php
+++ app/controllers/admin/pages/AdminPageController.php
@@ -9,6 +9,41 @@ class AdminPageController extends AdminController
         $this->layout->content = View::make('admin.page.list')->with('pages', $pages);
     }
 
+    public function create()
+    {
+        var_dump(Session::get('errors'));
+        $this->layout->content = View::make('admin.page.create');
+    }
+
+    public function store()
+    {
+        $validator = Validator::make(Input::input(), [
+            'title'        => 'required',
+            'slug'         => 'required|unique:pages,slug',
+            'page_content' => 'required'
+        ], [
+            'title.required'        => 'You must enter a page title.',
+            'slug.required'         => 'You must enter a page slug.',
+            'slug.unique'           => 'This slug is already in use.',
+            'page_content.required' => 'You must enter some page content.'
+        ]);
+
+        if ($validator->fails()) {
+            Input::flashExcept('_token', 'submit');
+            return Redirect::to('admin/page/create')->with('errors', $validator->messages()->toArray());
+        }
+
+        $page = new Page;
+
+        $page->title   = Input::input('title');
+        $page->slug    = Input::input('slug');
+        $page->content = Input::input('page_content');
+
+        $page->save();
+
+        return Redirect::to('/admin/page')->with('message', 'The page was added successfully. You can view it <a href="'.URL::to($page->slug).'">here</a>')->with('message-type', 'alert-success');
+    }
+
     public function edit($id)
     {
         $page = Page::findOrFail($id);
---
+++ app/views/admin/page/create.blade.php
@@ -0,0 +1,56 @@
+@section("title")
+Edit Page | BeardCMS ACP
+@endsection
+
+@section("content")
+<div class="container">
+    <div class="row">
+        <div class="col-12 well">
+            <fieldset>
+                <legend>Create a new page</legend>
+                @if(Session::has('errors'))
+                @foreach(Session::get('errors') as $error)
+                <div class="alert alert-error">
+                    <strong>Error!</strong> <button type="button" class="close" data-dismiss="alert">&times;</button>
+                    @foreach($error as $message)
+                        {{ $message }} <br />
+                    @endforeach
+                </div>
+                @endforeach
+                @endif
+                {{ Form::open(array('method' => 'post', 'route' => ['admin.page.store'], 'class' => 'form-horizontal')) }}
+                <div class="form-group">
+                    <label class="col-lg-2 control-label" for="title">Title</label>
+                    <div class="col-lg-10">
+                        {{ Form::text('title', Input::old('title'), ['class' => 'form-control', 'placeholder' => 'E.G: Home...']) }}
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="col-lg-2 control-label" for="slug">Slug</label>
+                    <div class="col-lg-10">
+                        {{ Form::text('slug', Input::old('slug'), ['class' => 'form-control', 'placeholder' => 'home.html']) }}
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="col-lg-2 control-label" for="page_content">Content</label>
+                    <div class="col-lg-10">
+                        {{ Form::textarea('page_content', Input::old('content'), ['class' => 'form-control']) }}
+                    </div>
+                </div>
+                <button type="submit" name="submit" class="btn btn-info btn-block btn-large">Create Page</button>
+                {{ Form::close(); }}
+            </fieldset>
+        </div>
+    </div>
+</div>
+@endsection
+
+@section('js')
+<script src="{{ URL::asset('js/vendor/ckeditor/ckeditor.js') }}"></script>
+<script>
+    CKEDITOR.replace('page_content',  {
+        toolbar: [],
+        startupMode : 'source'
+    });
+</script>
+@endsection
