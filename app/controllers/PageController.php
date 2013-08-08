2b9a471557f966d35e1996d5786baa22dacd05c5
Adam Lavin <adam@lavoaster.co.uk>
2013-08-08 22:53:13 +0100

    Made the page property local in PageController

--- app/controllers/PageController.php
+++ app/controllers/PageController.php
@@ -3,19 +3,14 @@
 class PageController extends BaseController {
 
     /**
-     * @var Page
-     */
-    protected $page;
-
-    /**
      * Find and set the layout's content
      *
      * @param $slug string The slug to the page
      * @return void
      */
     public function showPage($slug) {
-        $this->page = Page::getPage(ltrim($slug, "/") ?: 'index.html');
-        $this->layout->content = View::make('templates.pages.default')->with('page', $this->page->getAttributes());
+        $page = Page::getPage(ltrim($slug, "/") ?: 'index.html');
+        $this->layout->content = View::make('templates.pages.')->with('page', $page->getAttributes());
     }
 
 }
