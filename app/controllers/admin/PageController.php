5a401ec46c8e62b3334b57c3298af3112abe814f
Adam Lavin <adam@lavoaster.co.uk>
2013-08-09 08:05:12 +0100

    Updated PageController::index to use admin.page.index

--- app/controllers/admin/PageController.php
+++ app/controllers/admin/PageController.php
@@ -16,9 +16,7 @@ class PageController extends AdminController
 
     public function index()
     {
-        $pages = Page::all();
-
-        $this->layout->content = View::make('admin.page.list')->with('pages', $pages);
+        $this->layout->content = View::make('admin.page.index')->with('pages', Page::all());
     }
 
     public function create()
