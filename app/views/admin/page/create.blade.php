85d328fc63f542d9c64fe3aa0b91aca2d158fbf1
Adam Lavin <adam@lavoaster.co.uk>
2013-08-12 01:15:03 +0100

    Updated create page view to match the new edit page view

--- app/views/admin/page/create.blade.php
+++ app/views/admin/page/create.blade.php
@@ -5,46 +5,76 @@ Create Page | BeardCMS ACP
 @section("content")
 <div class="container">
     <div class="row">
-        <div class="col-12 well">
+        <div class="col-12">
+            <h1>Create a new page.</h1>
+        </div>
+    </div>
+    <hr>
+    <div class="row">
+        <div class="col-12">
             <fieldset>
-                <legend>Create a new page</legend>
                 @if(Session::has('errors'))
                 @foreach(Session::get('errors') as $error)
                 <div class="alert alert-error">
                     <strong>Error!</strong> <button type="button" class="close" data-dismiss="alert">&times;</button>
                     @foreach($error as $message)
-                        {{ $message }} <br />
+                    {{ $message }} <br />
                     @endforeach
                 </div>
                 @endforeach
                 @endif
-                {{ Form::open(['method' => 'post', 'route' => ['admin.page.store'], 'class' => 'form-horizontal']) }}
-                <div class="form-group">
-                    <label class="col-lg-2 control-label" for="title">Title</label>
-                    <div class="col-lg-10">
-                        {{ Form::text('title', Input::old('title'), ['class' => 'form-control', 'placeholder' => 'E.G: Home...']) }}
+                {{ Form::model(['method' => 'put', 'route' => ['admin.page.store']]) }}
+                <div class="row">
+                    <div class="col-lg-8 form-horizontal">
+                        <div class="form-group">
+                            <label class="col-lg-2 control-label" for="title">Title</label>
+                            <div class="col-lg-10">
+                                {{ Form::text('title', Input::old('title'), ['id' => 'title', 'class' => 'form-control', 'placeholder' => 'E.G: Home...']) }}
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-lg-2 control-label" for="page_content">Content</label>
+                            <div class="col-lg-10">
+                                {{ Form::textarea('page_content', Input::old('content'), ['id' => 'page_content','class' => 'form-control']) }}
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="panel">
+                            <div class="panel-heading">
+                                <i class="icon-wrench"></i> Page Settings
+                            </div>
+                            <div class="form-horizontal">
+                                <div class="form-group">
+                                    <label class="col-lg-3 control-label" for="slug">Slug</label>
+                                    <div class="col-lg-9">
+                                        {{ Form::text('slug', Input::old('slug'), ['id' => 'slug', 'class' => 'form-control', 'placeholder' => 'home.html']) }}
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="col-lg-3 control-label" for="template">Template</label>
+                                    <div class="col-lg-9">
+                                        {{ Form::select('template', $templates, Input::old('template') , ['class' => 'form-control']) }}
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="col-lg-3 control-label" for="template">Visibility</label>
+                                    <div class="col-lg-9">
+                                        {{ Form::select('visibility', [
+                                        'public'  => 'Public',
+                                        'private' => 'Private'
+                                        ], Input::old('visibility') ?: null, ['class' => 'form-control']) }}
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="panel-footer">
+                                <button type="submit" name="submit" class="ladda-button btn btn-info btn-block btn-large" data-style="slide-up">Create Page</button>
+                            </div>
+                        </div>
+                        <hr>
                     </div>
                 </div>
-                <div class="form-group">
-                    <label class="col-lg-2 control-label" for="slug">Slug</label>
-                    <div class="col-lg-10">
-                        {{ Form::text('slug', Input::old('slug'), ['class' => 'form-control', 'placeholder' => 'home.html']) }}
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-lg-2 control-label" for="template">Template</label>
-                    <div class="col-lg-10">
-                        {{ Form::select('template', $templates, Input::old('template'),['class' => 'form-control']) }}
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-lg-2 control-label" for="page_content">Content</label>
-                    <div class="col-lg-10">
-                        {{ Form::textarea('page_content', Input::old('content'), ['id' => 'page_content', 'class' => 'form-control']) }}
-                    </div>
-                </div>
-                <button type="submit" name="submit"class="ladda-button btn btn-info btn-block btn-large" data-style="slide-up">Create Page</button>
-                {{ Form::close() }}
+                {{ Form::close(); }}
             </fieldset>
         </div>
     </div>
@@ -56,7 +86,7 @@ Create Page | BeardCMS ACP
 <script src="{{ URL::asset('js/vendor/ckeditor/adapters/jquery.js') }}"></script>
 <script>
     $('#page_content').ckeditor({
-        toolbar: [],
+        toolbar: [{ name: 'sourceTools', items: ['searchCode','autoFormat','CommentSelectedRange','UncommentSelectedRange','AutoComplete']}, { name: 'maximize', items: [ 'Maximize' ] },],
         startupMode : 'source'
     });
 </script>
